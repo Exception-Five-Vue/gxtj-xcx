76ffea1fb37385f17aff2713097f696035bd459b
YannPro <849723885@qq.com>
2018-04-10 21:41:42 +0800

    init commit

---
+++ mock/information.js
@@ -0,0 +1,426 @@
+module.exports = {
+  "status": 1,
+  "message": "实时热点>>>>>推送>>>>>成功",
+  "result": [
+    {
+      "id": 29224,
+      "infoId": 29525,
+      "title": "游侠汽车工厂落址湖州，年底发布量产车游侠X",
+      "author": "钛媒体",
+      "description": "钛媒体快讯 | 4月3日消息： 游侠汽车湖州工厂正式启动。同时，游侠官方也确认，将于今年四季度发布首款全新量产车型游侠X。         据悉，游侠工厂由中国汽车工业工程有限公司设计规划，该工厂将完全针对纯电动汽车产线平台进行全新设计建设，首次采用与宝马工厂一致的全廊道传输设计，太阳能屋顶列阵分布",
+      "reads": 1,
+      "likes": 0,
+      "keyword": "游侠,汽车,工厂,设计,正式,媒体,产品,进行,建设,建设,",
+      "score": 0,
+      "sourceUrl": "http://www.tmtpost.com/3167178.html",
+      "sourceSite": "钛媒体",
+      "publishDate": 1522684800000,
+      "infoContent": null,
+      "infoImage": {
+        "id": 10548,
+        "infoId": 29525,
+        "image": "http://hta.hengtiansoft.com/image/group1/M00/2B/09/fwAAAVrDdAKAWo7BAALBbRQqEsk11.jpeg"
+      },
+      "read": false
+    },
+    {
+      "id": 29241,
+      "infoId": 29542,
+      "title": "出行市场爆发第二次世界大战",
+      "author": "王糈",
+      "description": "今日，美团点评将以37亿美元收购摩拜的消息不胫而走，这让原本焦灼的出行市场，再次躁动起来。          据蓝鲸财经报道，摩拜将以37亿美元的总价出售给美团， 包括27亿美元的实际作价（12亿美元现金及15亿美元股权）和10亿美元的债务。         美团和摩拜双方对钛媒体表示，不予置评。 ",
+      "reads": 0,
+      "likes": 0,
+      "keyword": "市场,出行,单车,报道,共享,方面,投资,ofo,媒体,媒体,",
+      "score": 0,
+      "sourceUrl": "http://www.tmtpost.com/3166943.html",
+      "sourceSite": "钛媒体",
+      "publishDate": 1522684800000,
+      "infoContent": null,
+      "infoImage": {
+        "id": 10565,
+        "infoId": 29542,
+        "image": "http://hta.hengtiansoft.com/image/group1/M00/2A/FA/fwAAAVrDPEmAAA-1AAFLENVIpqM29.jpeg"
+      },
+      "read": false
+    },
+    {
+      "id": 29262,
+      "infoId": 29563,
+      "title": "传美团37亿美元股权投资摩拜，滴滴同意ofo债转股",
+      "author": "钛媒体",
+      "description": "题图来源：视觉中国          钛媒体快讯 | 4月3日消息： 据财新网报道，近日，在腾讯董事会主席兼CEO马化腾的牵线之下，美团点评正式谈妥，以较大比例股权投资入股摩拜单车，美团点评与滴滴的将战场扩展至共享单车领域。         钛媒体向双方求证此事，双方均表示不予置评。        ",
+      "reads": 1,
+      "likes": 0,
+      "keyword": "投资,媒体,双方,单车,报道,正式,运营,近日,同意,同意,",
+      "score": 0,
+      "sourceUrl": "http://www.tmtpost.com/3166644.html",
+      "sourceSite": "钛媒体",
+      "publishDate": 1522684800000,
+      "infoContent": null,
+      "infoImage": {
+        "id": 10586,
+        "infoId": 29563,
+        "image": "http://hta.hengtiansoft.com/image/group1/M00/2A/E9/fwAAAVrDARSAL-hmAAGuaH5i-nk43.jpeg"
+      },
+      "read": false
+    },
+    {
+      "id": 29263,
+      "infoId": 29564,
+      "title": "外媒称Model 3周产能达2000辆，马斯克住进工厂管理生产",
+      "author": "李勤",
+      "description": "据外媒Electrek报道，特斯拉CEO Elon Musk在发给公司内部的一封邮件中表示，Model 3的产量现在已超过每周2,000个单位。         Elon Musk写道：                    “Model 3每周达到2000辆产能的难度非常高，但我们终于做到了。按照",
+      "reads": 3,
+      "likes": 0,
+      "keyword": "Model,Musk,工厂,表示,生产,公司,车型,瓶颈,数字,数字,",
+      "score": 0,
+      "sourceUrl": "http://www.tmtpost.com/3166587.html",
+      "sourceSite": "钛媒体",
+      "publishDate": 1522684800000,
+      "infoContent": null,
+      "infoImage": {
+        "id": 10587,
+        "infoId": 29564,
+        "image": "http://hta.hengtiansoft.com/image/group1/M00/2A/E9/fwAAAVrDAQqAOY0QAAFo69HHpBk90.jpeg"
+      },
+      "read": false
+    },
+    {
+      "id": 29264,
+      "infoId": 29565,
+      "title": "【钛晨报】京东回应被五部门约谈：接受批评，全面整改",
+      "author": "钛媒体",
+      "description": "【钛媒体综合】4月2日，北京市网信办、北京市公安局、北京市通信管理局、北京市工商局、北京市文化市场行政执法总队就京东在网上售卖相关违法违规商品、出版物及其他印刷品的行为，依法约谈网站相关负责人，并责令整改。         对此，京东回应称，认真接受批评，全面严肃整改。         据网信北京披",
+      "reads": 0,
+      "likes": 0,
+      "keyword": "相关,体育,京东,公司,媒体,金立,内容,整改,用户,用户,",
+      "score": 0,
+      "sourceUrl": "http://www.tmtpost.com/3165622.html",
+      "sourceSite": "钛媒体",
+      "publishDate": 1522684800000,
+      "infoContent": null,
+      "infoImage": {
+        "id": 10588,
+        "infoId": 29565,
+        "image": "http://hta.hengtiansoft.com/image/group1/M00/2A/E2/fwAAAVrCyEiAMgGHAAC64tORyzY82.jpeg"
+      },
+      "read": false
+    },
+    {
+      "id": 29460,
+      "infoId": 29761,
+      "title": "《头号玩家》：一封献给极客的情书",
+      "author": "极客电影Geekmovie",
+      "description": "知乎上 “如  何评价电影《头号玩家》?” ，这样一个答案引起我注意：                    我的前半生三十五年，花了几万个小时，看了那么多的电影动漫，玩了那么多的游戏，这期间顶着父母、老师、女朋友、老婆的白眼与打压，无数人说我幼稚，说我浪费生命，但总觉得，我付出的这么多时间与精力，",
+      "reads": 0,
+      "likes": 0,
+      "keyword": "电影,世界,利迪,游戏,绿洲,伯格,斯皮,觉得,片中,片中,",
+      "score": 0,
+      "sourceUrl": "http://www.tmtpost.com/3164602.html",
+      "sourceSite": "钛媒体",
+      "publishDate": 1522598400000,
+      "infoContent": null,
+      "infoImage": {
+        "id": 10784,
+        "infoId": 29761,
+        "image": "http://hta.hengtiansoft.com/image/group1/M00/2A/90/fwAAAVrB5_2AcOAiAANopM-ZPSk72.jpeg"
+      },
+      "read": false
+    },
+    {
+      "id": 29461,
+      "infoId": 29762,
+      "title": "5年投资150亿元，奇点汽车全球研发中心落地苏州相城",
+      "author": "钛极客",
+      "description": "传统汽车产业的发展离不开政府的战略扶持，智能汽车也难以违背这个大环境。         3月31日，在苏州相城（北京）科技投资推介会上，新造车势力之一的奇点汽车宣布将与苏州市、相城区以及高铁新城开展全方位的合作。         根据奇点汽车CEO沈海寅公布的消息，奇点汽车将在未来5年投资150亿元",
+      "reads": 1,
+      "likes": 0,
+      "keyword": "汽车,研发,技术,智能,苏州,生产,投资,产业,制造,制造,",
+      "score": 0,
+      "sourceUrl": "http://www.tmtpost.com/3164746.html",
+      "sourceSite": "钛媒体",
+      "publishDate": 1522598400000,
+      "infoContent": null,
+      "infoImage": {
+        "id": 10785,
+        "infoId": 29762,
+        "image": "http://hta.hengtiansoft.com/image/group1/M00/2A/90/fwAAAVrB59iAPTlmAAD8KMpuWZU89.jpeg"
+      },
+      "read": false
+    },
+    {
+      "id": 29225,
+      "infoId": 29526,
+      "title": "沉默，是一个人的气度",
+      "author": "洞见",
+      "description": "洞见   （DJ00123987）      ——不一样的观点，不一样的故事，数百万人订阅的微信大号。点击标题下蓝字“洞见”免费关注，我们将为您提供有价值、有意思的延伸阅读。               作者：芳草伊人       来源：水玲珑美文   （ID: sll221144）        ",
+      "reads": 8,
+      "likes": 11,
+      "keyword": "沉默,表现,气度,解释,关注,选择,做到,朋友,修养,修养,",
+      "score": 0,
+      "sourceUrl": "http://mp.weixin.qq.com/s?__biz=MjM5MDc0NTY2OA==&mid=2651434102&idx=6&sn=950fac70d8880546854fb5ca7439afa7&scene=0#wechat_redirect",
+      "sourceSite": "洞见",
+      "publishDate": 1522598400000,
+      "infoContent": null,
+      "infoImage": {
+        "id": 10549,
+        "infoId": 29526,
+        "image": "http://hta.hengtiansoft.com/image/group1/M00/2B/06/fwAAAVrDb_yAPg4JAABrG3hOzDM57.jpeg"
+      },
+      "read": false
+    },
+    {
+      "id": 29226,
+      "infoId": 29527,
+      "title": "丈夫拒绝产妇无痛分娩：女人生孩子才知道自己嫁的是人还是狗",
+      "author": "洞见",
+      "description": "洞见   （DJ00123987）      ——不一样的观点，不一样的故事，数百万人订阅的微信大号。点击标题下蓝字“洞见”免费关注，我们将为您提供有价值、有意思的延伸阅读。               作者：牛油果       来源：牛油果说   （ID: znsnlj）        生了孩子，",
+      "reads": 5,
+      "likes": 0,
+      "keyword": "孩子,丈夫,没有,知道,男人,人生,女人,张先生,妻子,妻子,",
+      "score": 0,
+      "sourceUrl": "http://mp.weixin.qq.com/s?__biz=MjM5MDc0NTY2OA==&mid=2651434102&idx=8&sn=fc80bf14b9f218381ea093122279e91d&scene=0#wechat_redirect",
+      "sourceSite": "洞见",
+      "publishDate": 1522598400000,
+      "infoContent": null,
+      "infoImage": {
+        "id": 10550,
+        "infoId": 29527,
+        "image": "http://hta.hengtiansoft.com/image/group1/M00/2B/06/fwAAAVrDb-2AFNQDAABV7_W5hL451.jpeg"
+      },
+      "read": false
+    },
+    {
+      "id": 29227,
+      "infoId": 29528,
+      "title": "什么都不信，可能是见识太少",
+      "author": "洞见",
+      "description": "洞见   （DJ00123987）      ——不一样的观点，不一样的故事，数百万人订阅的微信大号。点击标题下蓝字“洞见”免费关注，我们将为您提供有价值、有意思的延伸阅读。               作者：祝小兔       来源：好好虚度时光   （ID: hhxdsg）        人生路",
+      "reads": 2,
+      "likes": 0,
+      "keyword": "相信,见识,故事,生活,世界,知更鸟,艺术,轻易,觉得,轻易,",
+      "score": 0,
+      "sourceUrl": "http://mp.weixin.qq.com/s?__biz=MjM5MDc0NTY2OA==&mid=2651434102&idx=4&sn=99495bba77da04a004a4ef9d0e04dc7a&scene=0#wechat_redirect",
+      "sourceSite": "洞见",
+      "publishDate": 1522598400000,
+      "infoContent": null,
+      "infoImage": {
+        "id": 10551,
+        "infoId": 29528,
+        "image": "http://hta.hengtiansoft.com/image/group1/M00/2B/06/fwAAAVrDb-yAHbOnAACciRkLrlU71.jpeg"
+      },
+      "read": false
+    },
+    {
+      "id": 29228,
+      "infoId": 29529,
+      "title": "86岁老奶奶6000万建瓷宫：钱花在哪儿，最能体现一个人的层次",
+      "author": "洞见",
+      "description": "洞见   （DJ00123987）      ——不一样的观点，不一样的故事，数百万人订阅的微信大号。点击标题下蓝字“洞见”免费关注，我们将为您提供有价值、有意思的延伸阅读。              作者：洞见Grape        一个人的修养和层次，隐藏在他们对待金钱的态度里。       ",
+      "reads": 2,
+      "likes": 0,
+      "keyword": "老人,生活,父亲,修养,一直,余二妹,没有,卢永根,金钱,金钱,",
+      "score": 0,
+      "sourceUrl": "http://mp.weixin.qq.com/s?__biz=MjM5MDc0NTY2OA==&mid=2651434102&idx=2&sn=847b15b9defa0fd21850f13b0709a238&scene=0#wechat_redirect",
+      "sourceSite": "洞见",
+      "publishDate": 1522598400000,
+      "infoContent": null,
+      "infoImage": {
+        "id": 10552,
+        "infoId": 29529,
+        "image": "http://hta.hengtiansoft.com/image/group1/M00/2B/06/fwAAAVrDb8aAR0zzAAB1OhIpMGc61.jpeg"
+      },
+      "read": false
+    },
+    {
+      "id": 29229,
+      "infoId": 29530,
+      "title": "别人家的公司！看顺丰和德邦如何变着花样爱员工",
+      "author": "洞见",
+      "description": "洞见   （DJ00123987）      ——不一样的观点，不一样的故事，数百万人订阅的微信大号。点击标题下蓝字“洞见”免费关注，我们将为您提供有价值、有意思的延伸阅读。           自从网上有“佛系”甲方后，相继被发现很多佛系人。比如佛系媳妇儿，佛系乙方，佛系老师，佛系微商。当然，还有",
+      "reads": 3,
+      "likes": 1,
+      "keyword": "员工,快递,老板,价值,公司,崔维,爱护,企业,服务,服务,",
+      "score": 0,
+      "sourceUrl": "http://mp.weixin.qq.com/s?__biz=MjM5MDc0NTY2OA==&mid=2651434102&idx=1&sn=28732bc2f490dcc1c02c427f75a54b5b&scene=0#wechat_redirect",
+      "sourceSite": "洞见",
+      "publishDate": 1522598400000,
+      "infoContent": null,
+      "infoImage": {
+        "id": 10553,
+        "infoId": 29530,
+        "image": "http://hta.hengtiansoft.com/image/group1/M00/2B/06/fwAAAVrDb8aAJmMvAATTFIEHEts843.gif"
+      },
+      "read": false
+    },
+    {
+      "id": 29485,
+      "infoId": 29786,
+      "title": "污力滔滔的香蕉先生，竟然还可以用来做这种事",
+      "author": "钛空",
+      "description": "空少说： 昨天是愚人节，啊啊啊~有被整咩？          现在的整蛊套路层出不穷，防不胜防，大家可要注意防范，不然一不小心就会被笑话到明年。                   或者有些不屑于玩套路的，简单粗暴系列爱好者...大家一定要注意脚下的路。# 套路是不会的，这辈子都不会的，所以直接把你",
+      "reads": 1,
+      "likes": 0,
+      "keyword": "香蕉,使用,头发,邪恶,BANANA,SIR,先生,非常,朋友,朋友,",
+      "score": 0,
+      "sourceUrl": "http://www.tmtpost.com/3163092.html",
+      "sourceSite": "钛媒体",
+      "publishDate": 1522598400000,
+      "infoContent": null,
+      "infoImage": {
+        "id": 10809,
+        "infoId": 29786,
+        "image": "http://hta.hengtiansoft.com/image/group1/M00/2A/81/fwAAAVrBr0SASiaLAAYnS79cN0c463.gif"
+      },
+      "read": false
+    },
+    {
+      "id": 29230,
+      "infoId": 29531,
+      "title": "2046，这个世界会好吗？",
+      "author": "新周刊",
+      "description": "人工智能超越人类智能的“奇点”即将到来， 你准备好了吗？         开始的时候，我们创造工具，后来它们造就我们。（马歇尔·麦克卢汉）           ——————          这是知乎用户“大脑袋刀客”讲的一个段子：              某公司前台，年轻漂亮的前台姑娘微笑着问访",
+      "reads": 0,
+      "likes": 7,
+      "keyword": "人类,人工智能,机器人,世界,智能,未来,现实,机械,虚拟,虚拟,",
+      "score": 0,
+      "sourceUrl": "http://mp.weixin.qq.com/s?__biz=MjM5ODMzMDMyMw==&mid=2653187945&idx=1&sn=a7d420b0646d4e636db963fbb9213ee4&scene=0#wechat_redirect",
+      "sourceSite": "新周刊",
+      "publishDate": 1522598400000,
+      "infoContent": null,
+      "infoImage": {
+        "id": 10554,
+        "infoId": 29531,
+        "image": "http://hta.hengtiansoft.com/image/group1/M00/2B/05/fwAAAVrDb3CAQU71AAIIx5VgZbI677.gif"
+      },
+      "read": false
+    },
+    {
+      "id": 29486,
+      "infoId": 29787,
+      "title": "Louis Vuitton更换设计师，潮牌Off",
+      "author": "RET睿意德商业地产",
+      "description": "法国奢侈品牌Louis Vuitton在26日宣布， 高端街头品牌Off-White主理人Virgil Abloh成为其新一任男装艺术总监。在此之前，LV男装设计师Kim Jones已经执掌七年之久，接下来Kim Jones将继位LVMH去年收购的百年品牌Dior。         作为LouisV",
+      "reads": 0,
+      "likes": 0,
+      "keyword": "品牌,奢侈,市场,LV,消费者,设计师,奢侈品,文化,Abloh,abloh,",
+      "score": 0,
+      "sourceUrl": "http://www.tmtpost.com/3164544.html",
+      "sourceSite": "钛媒体",
+      "publishDate": 1522598400000,
+      "infoContent": null,
+      "infoImage": {
+        "id": 10810,
+        "infoId": 29787,
+        "image": "http://hta.hengtiansoft.com/image/group1/M00/2A/81/fwAAAVrBr0yALJpDAAP4KgQERQA54.jpeg"
+      },
+      "read": false
+    },
+    {
+      "id": 29231,
+      "infoId": 29532,
+      "title": "健康 | 春季过敏自救指南来袭，和喷嚏、皮肤瘙痒说拜拜！",
+      "author": "西安XIAN",
+      "description": "春已至，万物复苏草木发芽     在这个美丽的季节里     随处都是花香翠柳               可这些诱人的景色     却暗藏了很多危险               事件回顾                     ※  一位年轻女孩外出游玩，回家后双眼很痒，揉了一会儿双眼通红，被确诊",
+      "reads": 0,
+      "likes": 0,
+      "keyword": "过敏,花粉,皮肤,春季,外出,出现,导致,附着,过敏症,过敏症,",
+      "score": 0,
+      "sourceUrl": "http://mp.weixin.qq.com/s?__biz=MjM5ODM4OTY5Mw==&mid=2649903807&idx=4&sn=9dbaca2e54d6d131a753b464c97ec8d9&scene=0#wechat_redirect",
+      "sourceSite": "西安XIAN",
+      "publishDate": 1522598400000,
+      "infoContent": null,
+      "infoImage": {
+        "id": 10555,
+        "infoId": 29532,
+        "image": "http://hta.hengtiansoft.com/image/group1/M00/2B/03/fwAAAVrDbe2AL7UoAAA2Dlxv4JE54.jpeg"
+      },
+      "read": false
+    },
+    {
+      "id": 29487,
+      "infoId": 29788,
+      "title": "拟出售传言再起，慢调子的家乐福中国陷中年危机",
+      "author": "刘旷",
+      "description": "题图来自：视觉中国         据赢商网监测，2017年，超市闭店数量明显增长，沃尔玛、家乐福、永旺等7个超市品牌共关闭45家门店，同比上涨28.57%。而在2018年第一季度进入尾声之际，传言家乐福蚌埠万达店即将关闭，该店是其在蚌埠最后一家门店。此前，家乐福已于1月12日，3月8日接连关闭了遂",
+      "reads": 0,
+      "likes": 0,
+      "keyword": "家乐福,零售,沃尔玛,模式,市场,消费者,超市,合作,传统,传统,",
+      "score": 0,
+      "sourceUrl": "http://www.tmtpost.com/3164513.html",
+      "sourceSite": "钛媒体",
+      "publishDate": 1522598400000,
+      "infoContent": null,
+      "infoImage": {
+        "id": 10811,
+        "infoId": 29788,
+        "image": "http://hta.hengtiansoft.com/image/group1/M00/2A/81/fwAAAVrBr0eAQUyhAAcwZbA5134081.png"
+      },
+      "read": false
+    },
+    {
+      "id": 29232,
+      "infoId": 29533,
+      "title": "实用 | 一点食盐就能解决，你却花了那么多冤枉钱！",
+      "author": "西安XIAN",
+      "description": "食盐除去是很好的调味品     其实还有着很多非同凡响的功效     赶快一起来了解一下吧     这些小窍门你绝对不知道！                    医 疗 篇            1、治疗脚气            用盐抹在脚尖、脚趾之间     能够杀菌消炎     用手揉捏5-",
+      "reads": 1,
+      "likes": 0,
+      "keyword": "食盐,有效,清洁,水果,作用,防止,能够,里加,火焰,火焰,",
+      "score": 0,
+      "sourceUrl": "http://mp.weixin.qq.com/s?__biz=MjM5ODM4OTY5Mw==&mid=2649903807&idx=5&sn=b74d0f0cc98cf4cf42aca139c93c8a94&scene=0#wechat_redirect",
+      "sourceSite": "西安XIAN",
+      "publishDate": 1522598400000,
+      "infoContent": null,
+      "infoImage": {
+        "id": 10556,
+        "infoId": 29533,
+        "image": "http://hta.hengtiansoft.com/image/group1/M00/2B/03/fwAAAVrDbe2AWyE8AAA2Dlxv4JE96.jpeg"
+      },
+      "read": false
+    },
+    {
+      "id": 29488,
+      "infoId": 29789,
+      "title": "快乐购披露芒果系财务状况：芒果TV扭亏为盈，2017年净利润4.89亿元",
+      "author": "文娱商业观察",
+      "description": "题图来自：视觉中国         芒果系5家企业整体注入上市公司快乐购有了最新进展。         昨晚（4月1日），上市公司快乐购一口气发布了20多条公告，其中绝大部分都是关于拟注入上市公司的修改方案以及5家公司最新的财务数据。         相比较于第一版本的数据来看，这一次修订的内容主要",
+      "reads": 0,
+      "likes": 0,
+      "keyword": "芒果,艺人,公司,TV,收入,传媒,业务,增长,快乐,快乐,",
+      "score": 0,
+      "sourceUrl": "http://www.tmtpost.com/3163495.html",
+      "sourceSite": "钛媒体",
+      "publishDate": 1522598400000,
+      "infoContent": null,
+      "infoImage": {
+        "id": 10812,
+        "infoId": 29789,
+        "image": "http://hta.hengtiansoft.com/image/group1/M00/2A/81/fwAAAVrBrzmAXxCCAAjR7h8AHc0743.png"
+      },
+      "read": false
+    },
+    {
+      "id": 29233,
+      "infoId": 29534,
+      "title": "人物 | 外卖小哥在《中国诗词大会》上火了！烦恼的是没空送外卖了！",
+      "author": "西安XIAN",
+      "description": "“              每一个人也都可能，       在你的跟前变成一个传奇。           ”       这是北京师范大学文学院教授康震对一位外卖小哥的评价。那么，究竟是谁能获得一位诗词方面颇有建树的文学教授的称赞？      答案是——  雷海为，一位来自杭州的外卖小哥  。和其",
+      "reads": 0,
+      "likes": 0,
+      "keyword": "诗词,雷海为,雷海,诗意,大会,中国,舞台,遗憾,之中,遗憾,",
+      "score": 0,
+      "sourceUrl": "http://mp.weixin.qq.com/s?__biz=MjM5ODM4OTY5Mw==&mid=2649903807&idx=3&sn=7fb14f0fcf1dccc08aa5f3e41521cf84&scene=0#wechat_redirect",
+      "sourceSite": "西安XIAN",
+      "publishDate": 1522598400000,
+      "infoContent": null,
+      "infoImage": {
+        "id": 10557,
+        "infoId": 29534,
+        "image": "http://hta.hengtiansoft.com/image/group1/M00/2B/03/fwAAAVrDbTiAcXLrAAE6htyNULM16.jpeg"
+      },
+      "read": false
+    }
+  ]
+}
---
+++ mock/type.js
@@ -0,0 +1,34 @@
+module.exports = {
+  "status": 1,
+  "message": "获取类别列表--成功",
+  "result": [
+    {
+      "typeId": 1,
+      "typeName": "娱乐"
+    },
+    {
+      "typeId": 2,
+      "typeName": "法律"
+    },
+    {
+      "typeId": 3,
+      "typeName": "金融"
+    },
+    {
+      "typeId": 4,
+      "typeName": "科技"
+    },
+    {
+      "typeId": 5,
+      "typeName": "互联网"
+    },
+    {
+      "typeId": 6,
+      "typeName": "政治"
+    },
+    {
+      "typeId": 7,
+      "typeName": "生活"
+    }
+  ]
+}
